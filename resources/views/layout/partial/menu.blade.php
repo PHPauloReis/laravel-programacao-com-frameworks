fe661143d8cc2a78f574d514ead68b389684b8f9
Paulo Reis <pauloreisprofissional@gmail.com>
2022-11-01 22:01:17 -0300

    Corrigindo rota e usando padrão em inglês para testar

--- resources/views/layout/partial/menu.blade.php
+++ resources/views/layout/partial/menu.blade.php
@@ -10,7 +10,7 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="veiculos">Veiculos</a>
+          <a class="nav-link" href="{{ route('veiculos.index') }}">Veiculos</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{ route('veiculos.create') }}">Novo Veiculo</a>
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [VeiculoController::class, 'index']);
 
-Route::resource('/veiculos', VeiculoController::class);
+Route::resource('/vehicles', VeiculoController::class)
+    ->names('veiculos')
+    ->parameters(['vehicles' => 'veiculo']);;
